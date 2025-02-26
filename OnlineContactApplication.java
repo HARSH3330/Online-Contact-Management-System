a3fbb5345afb6b7855f0fbd2bd2ca63da301528e
HARSH3330 <harshagarwal244@gmail.com>
2025-02-26 10:11:58 +0530

    Create OnlineContactApplication.java

---
+++ OnlineContactApplication.java
@@ -0,0 +1,13 @@
+package com.example.OnlineContact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineContactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineContactApplication.class, args);
+	}
+
+}
